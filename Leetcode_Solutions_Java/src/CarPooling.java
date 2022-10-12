55d2232c562171689b35ec743059a783bf02618a
Yu Zhang <yzhang0713@gmail.com>
2022-10-12 11:04:15 -0400

    Commit-2 CarPooling

---
+++ Leetcode_Solutions_Java/src/CarPooling.java
@@ -0,0 +1,56 @@
+import java.util.PriorityQueue;
+import java.util.Comparator;
+
+public class CarPooling {
+
+    public CarPooling() {
+
+    }
+
+    private class Passenger {
+        public int numPassengers;
+        public int from;
+        public int to;
+        public Passenger(int[] passengerInfo) {
+            this.numPassengers = passengerInfo[0];
+            this.from = passengerInfo[1];
+            this.to = passengerInfo[2];
+        }
+    }
+
+    private class PassengerByToComparator implements Comparator<Passenger> {
+        @Override
+        public int compare(Passenger p1, Passenger p2) {
+            return p1.to - p2.to;
+        }
+    }
+
+    private class PassengerByFromComparator implements Comparator<Passenger> {
+        @Override
+        public int compare(Passenger p1, Passenger p2) {
+            return p1.from - p2.from;
+        }
+    }
+
+    public boolean carPooling(int[][] trips, int capacity) {
+        int remainingCapacity = capacity;
+        PriorityQueue<Passenger> passengersToPickup = new PriorityQueue(8, new PassengerByFromComparator());
+        PriorityQueue<Passenger> passengersToDrop = new PriorityQueue(8, new PassengerByToComparator());
+        for (int i = 0; i < trips.length; i++) {
+            passengersToPickup.add(new Passenger(trips[i]));
+        }
+        while (passengersToPickup.size() > 0) {
+            Passenger nextPassenger = passengersToPickup.poll();
+            int nextPassengerLocation = nextPassenger.from;
+            while (passengersToDrop.size() > 0 && passengersToDrop.peek().to <= nextPassengerLocation) {
+                remainingCapacity += passengersToDrop.poll().numPassengers;
+            }
+            if (nextPassenger.numPassengers > remainingCapacity) {
+                return false;
+            }
+            passengersToDrop.add(nextPassenger);
+            remainingCapacity -= nextPassenger.numPassengers;
+        }
+        return true;
+    }
+}
---
+++ Leetcode_Solutions_Java/test/CarPoolingTest.java
@@ -0,0 +1,63 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class CarPoolingTest {
+
+    private static CarPooling carPooling;
+
+    @BeforeAll
+    public static void setup() {
+        carPooling = new CarPooling();
+    }
+
+    @Test
+    public void testNoPassenger() {
+        int[][] passengerInfos = new int[][]{};
+        int capacity = 1;
+        Assertions.assertTrue(carPooling.carPooling(passengerInfos, capacity));
+    }
+
+    @Test
+    public void testPerfectDrop() {
+        int[][] passengerInfos = {{2,1,2}, {2,2,3}, {2,3,4}};
+        int capacity = 2;
+        Assertions.assertTrue(carPooling.carPooling(passengerInfos, capacity));
+    }
+
+    @Test
+    public void testSimpleFail() {
+        int[][] passengerInfos = {{5,1,2}};
+        int capacity = 2;
+        Assertions.assertFalse(carPooling.carPooling(passengerInfos, capacity));
+    }
+
+    @Test
+    public void testMultiplePassengerSuccess() {
+        int[][] passengerInfos = {{2,1,10}, {3,2,4}, {2,5,13}, {1,7,10}, {3,12,15}};
+        int capacity = 5;
+        Assertions.assertTrue(carPooling.carPooling(passengerInfos, capacity));
+    }
+
+    @Test
+    public void testMultiplePassengerNotInOrderSuccess() {
+        int[][] passengerInfos = {{1,7,10}, {2,1,10}, {3,12,15}, {3,2,4}, {2,5,13}};
+        int capacity = 5;
+        Assertions.assertTrue(carPooling.carPooling(passengerInfos, capacity));
+    }
+
+    @Test
+    public void testMultiplePassengerFail() {
+        int[][] passengerInfos = {{2,1,10}, {3,2,4}, {3,5,13}, {1,7,10}, {3,12,15}};
+        int capacity = 5;
+        Assertions.assertFalse(carPooling.carPooling(passengerInfos, capacity));
+    }
+
+    @Test
+    public void testMultiplePassengerNotInOrderFail() {
+        int[][] passengerInfos = {{1,7,10}, {2,1,10}, {3,12,15}, {3,2,4}, {3,5,13}};
+        int capacity = 5;
+        Assertions.assertFalse(carPooling.carPooling(passengerInfos, capacity));
+    }
+
+}
